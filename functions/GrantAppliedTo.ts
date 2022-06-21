caeca54645dfaf32b4c2af9b52bbb78b612e4d20
Richik Chanda <richikchanda1999@gmail.com>
2022-06-21 11:11:44 +0530

    update: added workspace ID as a parameter

--- functions/GrantAppliedTo.ts
+++ functions/GrantAppliedTo.ts
@@ -4,20 +4,20 @@ import { OnChainEvent } from "../src/configs/events";
 import { work } from "../src/utils/work";
 
 async function grantAppliedTo(req: Request, res: Response) {
-    const { grantId, chain } = req.body
+    const { workspaceId, chain } = req.body
     if (chain === undefined) {
         res.status(400).json({ 'Error': 'Missing chain' })
     } else if (typeof chain !== "number") {
         res.status(401).json({ 'Error': 'Invalid chain' })
     } else if (SupportedChainId[chain] === undefined) {
         res.status(402).json({ 'Error': 'Unsupported chain' })
-    } else if (grantId === undefined) {
-        res.status(405).json({ 'Error': 'Missing grantId' })
-    } else if (typeof grantId !== "string") {
-        res.status(406).json({ 'Error': 'Invalid grantId' })
+    } else if (workspaceId === undefined) {
+        res.status(403).json({ 'Error': 'Missing workspaceId' })
+    } else if (typeof workspaceId !== "string") {
+        res.status(404).json({ 'Error': 'Invalid workspaceId' })
     } else {
         try {
-            const data = await work(OnChainEvent.GrantAppliedTo, chain, { grantId })
+            const data = await work(OnChainEvent.GrantAppliedTo, chain, { workspaceId })
             res.status(200).json(data)
         } catch (e) {
             res.status(500).json({ 'Error': e })
--- functions/ReviewerSubmittedReview.ts
+++ functions/ReviewerSubmittedReview.ts
@@ -4,16 +4,20 @@ import { OnChainEvent } from "../src/configs/events";
 import { work } from "../src/utils/work";
 
 async function reviewerSubmittedReview(req: Request, res: Response) {
-    const { chain } = req.body
+    const { workspaceId, chain } = req.body
     if (chain === undefined) {
         res.status(400).json({ 'Error': 'Missing chain' })
     } else if (typeof chain !== "number") {
         res.status(401).json({ 'Error': 'Invalid chain' })
     } else if (SupportedChainId[chain] === undefined) {
         res.status(402).json({ 'Error': 'Unsupported chain' })
+    } else if (workspaceId === undefined) {
+        res.status(403).json({ 'Error': 'Missing workspaceId' })
+    } else if (typeof workspaceId !== "string") {
+        res.status(404).json({ 'Error': 'Invalid workspaceId' })
     } else {
         try {
-            const data = await work(OnChainEvent.ReviewerSubmittedReview, chain)
+            const data = await work(OnChainEvent.ReviewerSubmittedReview, chain, {workspaceId})
             res.status(200).json(data)
         } catch (e) {
             res.status(500).json({ 'Error': e })
